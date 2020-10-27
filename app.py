1bbb0fbd738877f1a95c676492c0bf4a2046f03b
Sweetzia <marjoleintje@mail.com>
2020-10-27 09:40:41 +0000

    added recipes.html and use jinja blocks so childtemplate inherits html code from parent template

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-import os
-from flask import Flask
-
+import os 
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Lola's Cookbook"
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
