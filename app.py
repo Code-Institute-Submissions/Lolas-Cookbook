ae1ec0cee24eb78ada5dea75fa402b2b077c0b67
Sweetzia <marjoleintje@mail.com>
2020-10-29 08:45:35 +0000

    Created add_recipes template

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
+@app.route("/add_recipes")
+def add_recipes():
+    return render_template("add_recipes.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
