985d72a3172422a7065fdd82882d12ba6ef542ba
Sweetzia <marjoleintje@mail.com>
2020-11-02 09:18:07 +0000

    added a submit button, so the user can submit the form and add a recipe to the database

--- app.py
+++ app.py
@@ -26,8 +26,22 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
-@app.route("/add_recipes")
+@app.route("/add_recipes", methods-["GET", "POST"])
 def add_recipes():
+    if request.method == "POST":
+        recipes = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "family_story": request.form.get("family_story"),
+            "recipe_ingredients": request.form.getlist("recipe_ingredients"),
+            "cooking_instruction": request.form.get("cooking_instruction"),
+        }
+
+        mongo.db.recipes.insert_one(recipes)
+        flash("Recipe succesfully added")
+        return redirect(url_for("get_recipes"))
+        
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipes.html", categories=categories)
 
